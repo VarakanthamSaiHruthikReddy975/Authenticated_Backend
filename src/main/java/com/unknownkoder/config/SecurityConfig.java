64173c4f1a280588088b875e549b92c3ce69add6
VarakanthamSaiHruthikReddy975 <105242080+VarakanthamSaiHruthikReddy975@users.noreply.github.com>
2024-02-06 09:33:36 -0600

    Added the jwt encoder, RSAKeyAlgorithm and jwt decoder code

--- src/main/java/com/unknownkoder/config/SecurityConfig.java
+++ src/main/java/com/unknownkoder/config/SecurityConfig.java
@@ -10,10 +10,31 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder.JwkSourceReactiveJwtDecoderBuilder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.unknownkoder.utils.RSAKeyProperties;
 
 @Configuration
 public class SecurityConfig{
+    private final RSAKeyProperties keys;
+
+    
+
+    public SecurityConfig(RSAKeyProperties keys) {
+        this.keys = keys;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -30,9 +51,24 @@ public class SecurityConfig{
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http
         .csrf(csrf->csrf.disable())
-        .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        .authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/auth/**").permitAll();
+            auth.anyRequest().authenticated();
+        })
         .httpBasic().and()
         .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(keys.getPublicKey()).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(){
+        JWK jwk = new RSAKey.Builder(keys.getPublicKey()).privateKey(keys.getPrivateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 
 }
--- src/main/java/com/unknownkoder/controllers/AuthenticationController.java
+++ src/main/java/com/unknownkoder/controllers/AuthenticationController.java
@@ -3,8 +3,10 @@ package com.unknownkoder.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.unknownkoder.model.ApplicationUser;
+import com.unknownkoder.model.RegistrationDTO;
 import com.unknownkoder.service.AuthenticationService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,10 +20,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/auth")
 @CrossOrigin("*")
 public class AuthenticationController {
+    @Autowired
     private AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ApplicationUser registerUser(){
-        return authenticationService.registerUser("", "");
+    public ApplicationUser registerUser(@RequestBody RegistrationDTO registrationDTO){
+        return authenticationService.registerUser(registrationDTO.getUsername(), registrationDTO.getPassword());
     }
 }
